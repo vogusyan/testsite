de3552aa9d64ea87a02049888c3e924ebe3db889
VoGus <vogusyan@gmail.com>
2018-11-02 14:45:50 +0300

    Changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#!%#h@(=hi*-(e#ho6vt+)x+swvtnvxid(4%@i&(*=-$wjytk7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'vogusyan.pythonanywhere.com']
 
 
 # Application definition
