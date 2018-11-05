ec69fe7bee03f0300102ef4aa64d250c96282307
VoGus <vogusyan@gmail.com>
2018-11-05 14:45:04 +0300

    order

--- first/views.py
+++ first/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
- return render(request,'first/post_list.html', {})
+ posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+ return render(request,'first/post_list.html', {'posts': posts})
